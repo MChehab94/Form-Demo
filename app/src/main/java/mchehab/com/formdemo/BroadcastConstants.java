e9d8072512fa824b8411b67a0e9443e27bb9a4f5
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-10 15:06:40 +0200

    Added broadcast constants

---
+++ app/src/main/java/mchehab/com/formdemo/BroadcastConstants.java
@@ -0,0 +1,11 @@
+package mchehab.com.formdemo;
+
+/**
+ * Created by muhammadchehab on 11/10/17.
+ */
+
+public class BroadcastConstants {
+    public static final String JSON = "json";
+    public static final String IMAGE = "image";
+    public static final String JSON_POST = "json post";
+}
